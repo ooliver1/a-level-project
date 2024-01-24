f96ab6f3c9b9c5ffc457bf71c84201c7515571ca
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-01-24 19:46:49 +0000

    random colour for ui

---
+++ main_menu/menu.gd
@@ -0,0 +1,13 @@
+extends MarginContainer
+
+func _ready() -> void:
+	randomize()
+	var dir := DirAccess.open("res://UI/themes")
+	var files := PackedStringArray()
+	for file in dir.get_files():
+		if file.ends_with(".tres"):
+			files.append(file)
+	var length := len(files)
+	var random := randi() % length
+	var file := files[random]
+	theme = load("res://UI/themes/" + file)
