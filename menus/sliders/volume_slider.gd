37d71049e6c6580d06c40769880ba830da0f9cfb
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-02-27 15:55:42 +0000

    finish volume sliders

--- menus/sliders/volume_slider.gd
+++ menus/sliders/volume_slider.gd
@@ -1,11 +1,13 @@
 extends VBoxContainer
 
 @export var audio_bus_name: StringName
+# The index of the audio bus in all buses.
 @onready var audio_bus_index := AudioServer.get_bus_index(audio_bus_name)
 @onready var value_node: Label = %Value
 
-
 func _on_slider_value_changed(value: float):
-	print(value)
-	AudioServer.set_bus_volume_db(audio_bus_index, linear_to_db(value))
+	# Godot uses decihels, which is logorithmic. This function converts a number
+	# from 0-1 into decibels (-80 to 24dB)
+	var db := linear_to_db(value / 100)
+	AudioServer.set_bus_volume_db(audio_bus_index, db)
 	value_node.text = str(value) + "%"
