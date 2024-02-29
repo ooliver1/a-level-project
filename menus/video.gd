cc9911ad1e6a0b4eca82873ba6f45a3f7d9efc08
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-02-29 18:16:58 +0000

    continue video

--- menus/video.gd
+++ menus/video.gd
@@ -18,10 +18,31 @@ func _ready() -> void:
 		id = 2
 	display_mode.select(id)
 
+	# msaa_2d and msaa_3d are the same here
+	var antialiasing = get_viewport().msaa_2d
+	id = 0
+	if antialiasing == Viewport.MSAA_2X:
+		id = 1
+	elif antialiasing == Viewport.MSAA_4X:
+		id = 2
+	elif antialiasing == Viewport.MSAA_8X:
+		id = 3
+
 
 func _on_display_mode_item_selected(index: int) -> void:
-	pass # Replace with function body.
-
+	# 0: Windowed
+	# 1: Fullscreen
+	# 2: Fullscreen Borderless
+	if index == 0:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+	elif index == 1:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+	elif index == 2:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+		DisplayServer.window_set_size(DisplayServer.screen_get_size())
 
 func _on_anti_aliasing_item_selected(index: int) -> void:
 	pass # Replace with function body.
