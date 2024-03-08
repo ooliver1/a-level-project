f5e31ac1c6f8f0b4b11ff54f5cab4f58a4997fe5
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-03-08 23:23:40 +0000

    so much diff for nothing

--- courses/world.gd
+++ courses/world.gd
@@ -6,5 +6,6 @@ const BALL_BLUE = preload("res://courses/scenes/ball_blue.tscn")
 func _ready() -> void:
 	var ball: RigidBody3D = BALL_BLUE.instantiate()
 	add_child(ball)
-	ball.position = Vector3(0, 0.1, 0)
-	ball.apply_central_impulse(Vector3(0.7, 0, 0.7))
+	ball.position = Vector3(0, 0.2, 1)
+	await get_tree().create_timer(1).timeout
+	ball.apply_central_impulse(Vector3(1, 0, 0))
