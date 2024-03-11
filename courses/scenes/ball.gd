4885b1133f2d7329a3b02c67ca23e67d546469e8
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-03-11 00:15:20 +0000

    progress on bounce physics

--- courses/scenes/ball.gd
+++ courses/scenes/ball.gd
@@ -5,11 +5,17 @@ func _physics_process(delta: float) -> void:
 	var collision := move_and_collide(linear_velocity * delta)
 	if collision:
 		var normal := collision.get_normal()
-		print(normal)
+
+		# Keep the ball on the ground, not fall through.
 		if normal.y != 0:
 			linear_velocity.y = 0
 
+		# Bounce off walls.
 		if normal.x != 0 or normal.z != 0:
 			var new_velocity := linear_velocity.bounce(normal)
 			linear_velocity.x = new_velocity.x
 			linear_velocity.z = new_velocity.z
+
+	# Stop the ball rolling forever.
+	if linear_velocity.length() < 0.03:
+		linear_velocity = Vector3.ZERO
