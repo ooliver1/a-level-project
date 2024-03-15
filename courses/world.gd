74cd0489bed0858a60e35d6c5915fb3f944de558
Oliver Wilkes <oliverwilkes2006@icloud.com>
2024-03-15 11:42:26 +0000

    physics improvements

--- courses/world.gd
+++ courses/world.gd
@@ -6,6 +6,6 @@ const BALL_BLUE = preload("res://courses/scenes/ball_blue.tscn")
 func _ready() -> void:
 	var ball: RigidBody3D = BALL_BLUE.instantiate()
 	add_child(ball)
-	ball.position = Vector3(0, 0.2, 1)
+	ball.position = Vector3(1, 0.5, 5.5)
 	await get_tree().create_timer(1).timeout
-	ball.apply_central_impulse(Vector3(2, 0, 4))
+	ball.apply_central_impulse(Vector3(5, 0, 0))
